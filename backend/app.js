4bd78cd4b589af6f57f54e6fde5a63cffb2d1a84
vishalponia <vishalponiavp@gmail.com>
2026-08-17 19:23:24 +0530

    Fix CORS for Vercel frontend

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(
   cors({
     origin:["http://localhost:5173",
-    "https://ecommerce-web-liard-chi.vercel.app/"] ,
+    "https://ecommerce-web-liard-chi.vercel.app"] ,
     credentials: true,
   })
 );
